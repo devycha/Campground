ce6fa4a580e6058bc9326d012920e67bf924f1e7
dongjji <y2kdj9723@naver.com>
2021-07-19 21:42:54 +0900

    Day48 embed delete function and middleware

--- app.js
+++ app.js
@@ -137,6 +137,16 @@ app.post(
   })
 );
 
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page is not found", 404));
 });
