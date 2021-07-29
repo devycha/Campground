fabcadc6d7bb554f0f77af08e3bf052bb391e19e
dongjji <y2kdj9723@naver.com>
2021-07-30 03:10:56 +0900

    heroku

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ store.on("error", function (e) {
 const sessionOptions = {
   store,
   secret,
-  reesave: false,
+  resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
@@ -155,6 +155,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("serving on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`serving on port ${port}`);
 });
